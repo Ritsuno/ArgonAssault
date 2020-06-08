cd3553aa116a50cedccc2c6c3eb6032981d79b72
Ritsuno <ryanwallen22@gmail.com>
2020-06-07 23:17:00 -0400

    1

---
+++ ArgonAssault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        if (FindObjectsOfType<AudioSource>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(NextScene());
+    }
+
+    IEnumerator NextScene()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
